96809fff05b234a087a245d5745f9407b27eb194
LO <117568990+ixhd@users.noreply.github.com>
2023-04-28 19:55:08 +0200

    Update SaveManager.lua

--- Linoria/Addons/SaveManager.lua
+++ Linoria/Addons/SaveManager.lua
@@ -83,7 +83,7 @@ local SaveManager = {} do
 			return false, 'no config file is selected'
 		end
 
-		local fullPath = self.Folder .. '/Configs/' .. name .. '.al'
+		local fullPath = self.Folder .. '/Configs/' .. name .. '.json'
 
 		local data = {
 			objects = {}
@@ -116,7 +116,7 @@ local SaveManager = {} do
 			return false, 'no config file is selected'
 		end
 		
-		local file = self.Folder .. '/Configs/' .. name .. '.al'
+		local file = self.Folder .. '/Configs/' .. name .. '.json'
 		if not isfile(file) then return false, 'invalid file' end
 
 		local success, decoded = pcall(httpService.JSONDecode, httpService, readfile(file))
@@ -159,10 +159,10 @@ local SaveManager = {} do
 		local out = {}
 		for i = 1, #list do
 			local file = list[i]
-			if file:sub(-5) == '.al' then
+			if file:sub(-5) == '.json' then
 				-- i hate this but it has to be done ...
 
-				local pos = file:find('.al', 1, true)
+				local pos = file:find('.json', 1, true)
 				local start = pos
 
 				local char = file:sub(pos, pos)
